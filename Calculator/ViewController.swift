adaf850fa93fa03150ce8810f349792ad95e3f1c
Dinar Shakirov <139841080+bonycl@users.noreply.github.com>
2023-11-02 16:19:20 +0700

    setup UI, setup display label

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,18 +12,32 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+    private var isFinishedTypingNumber: Bool = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
-        //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label to a Double.")
+        }
+        
+        
+        
     }
 
-    
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
-        //What should happen when a number is entered into the keypad
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+            
+        }
     
     }
 
